bd57d00fe4fe92a640ef7e0c82c3599a690d2bbd
Abdul025 <ghafor_n@live.com>
2019-01-26 16:49:20 -0500

    Bootstrap added

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,13 @@
 /* Custom JavaScript goes here*/
+// IIFE --Immidiately Invoked Function Expression
+(function (){
+
+    function Start()
+    {
+        console.log('%c app Started...', 
+        "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
