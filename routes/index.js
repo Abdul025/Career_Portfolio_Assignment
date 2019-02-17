d7b728614651d5de201e60bae2bee396e8f0c2fc
Abdul025 <ghafor_n@live.com>
2019-02-16 21:34:39 -0500

    routing changes

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,26 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/contact', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
